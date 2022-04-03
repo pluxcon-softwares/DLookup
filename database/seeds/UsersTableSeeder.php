7fd1f04e73ebc57750f8dff562190d58cf7415bd
Philip Otoo <pluxcon@gmail.com>
2022-04-03 18:01:40 +0000

    UsersTableSeeder sample account updated

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,46 +16,18 @@ class UsersTableSeeder extends Seeder
     {
         User::create([
             'username'  =>  'user1',
-            'email'     => 'user1@localhost.test',
-            'password'  => 123456,
-            'wallet'    => 1000
+            'email'     => 'user1@app.com',
+            'password'  => 123456789,
+            'wallet'    => 0,
+            'active'    =>  1
         ]);
 
         User::create([
             'username'  =>  'user2',
-            'email'     => 'user2@localhost.test',
+            'email'     => 'user2@app.com',
             'password'  => 123456,
-            'wallet'    => 1000
-        ]);
-
-        User::create([
-            'username'  =>  'user3',
-            'email'     => 'user3@localhost.test',
-            'password'  => 123456,
-            'active'    => 0
-        ]);
-
-        User::create([
-            'username'  =>  'user4',
-            'email'     => 'user4@localhost.test',
-            'password'  => 123456,
+            'wallet'    => 0,
             'active'    => 1
-        ]);
-
-        User::create([
-            'username'  =>  'user5',
-            'email'     => 'user5@localhost.test',
-            'password'  => 123456,
-            'active'    => 1,
-            'wallet'    =>  0
-        ]);
-
-        User::create([
-            'username'  =>  'user6',
-            'email'     => 'user6@localhost.test',
-            'password'  => 123456,
-            'active'    => 1,
-            'wallet'    =>  6
         ]);
     }
 }
